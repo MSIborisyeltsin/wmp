f83403face0b5622932d4aa8713c14656e9d225a
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-06-17 18:34:46 +0700

    Create PacketField.cs

---
+++ Protocol/PacketField.cs
@@ -0,0 +1,9 @@
+namespace Protocol
+{
+    public class PacketField
+    {
+        public byte FieldID { get; set; }
+        public byte FieldSize { get; set; }
+        public byte[] Contents { get; set; }
+    }
+}
