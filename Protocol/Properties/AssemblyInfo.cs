2d41a5c1e30d23b7fe253ebf7f56f6a165354d9b
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-06-18 19:27:33 +0700

    Пасибо
    Собрано скриптом снисходительной кошки: Русскоязычный версия скрипта "Галя Шьюха"

---
+++ Protocol/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+/*
+    Compiled by "Рускоязычная версия "Галя Шьюха" script: http://aipolygonafqoca92jdz0awmf02nc80a2o2.i2p/vor/galya-shiuha.html.
+    My mom: indulgence_neko^._.^
+*/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*
+    Общие сведения о сборке контролируются с помощью следующего набора атрибутов.
+    Измените значения этих атрибутов,
+    чтобы изменить информацию, связанную со сборкой.
+*/
+[assembly: AssemblyTitle("WMP")]
+[assembly: AssemblyDescription("Wired Message Protocol")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WMP")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+/*
+    Установка для ComVisible значения false делает типы в этой сборке невидимыми для COM-компонентов.
+    Если вам нужно получить доступ к типу в этой сборке из COM,
+    установите для атрибута ComVisible значение true для этого типа.
+*/
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// [assembly: Guid("")]
+
+/*
+    Version information for an assembly consists of the following four values:
+
+      Major Version
+      Minor Version
+      Build Number
+      Revision
+
+    You can specify all the values or you can default the Build and Revision Numbers
+    by using the '*' as shown below:
+
+    [assembly: AssemblyVersion("1.0.*")]
+
+*/
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
