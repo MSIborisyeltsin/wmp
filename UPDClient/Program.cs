e0ba03330d860ffbb127109f53c1225a0c308fdc
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-06-24 19:36:00 +0700

    Маин

---
+++ UPDClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UPDServer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Server";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var server = new Server();
+            server.Start();
+            server.AcceptClients();
+        }
+    }
+}
