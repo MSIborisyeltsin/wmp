5a7e0cc5ac8df8722253c3fe9e828bf19d649cdf
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-06-24 12:34:11 +0700

    Информация о сборке
    Галя Шьюха от Снисходительной_кошки

---
+++ UPDProtocol/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+/*
+    Compiled by "Рускоязычная версия "Галя Шьюха" script: http://aipolygonafqoca92jdz0awmf02nc80a2o2.i2p/vor/galya-shiuha.html.
+    My mom: indulgence_neko^._.^
+*/
+
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*
+    Общие сведения о сборке контролируются с помощью следующего набора атрибутов.
+    Измените значения этих атрибутов,
+    чтобы изменить информацию, связанную со сборкой.
+*/
+
+[assembly: AssemblyTitle("UPDServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UPDServer")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+/*
+    Установка для ComVisible значения false делает типы в этой сборке невидимыми для COM-компонентов.
+    Если вам нужно получить доступ к типу в этой сборке из COM,
+    установите для атрибута ComVisible значение true для этого типа.
+*/
+[assembly: ComVisible(false)]
+
+// Следующий GUID предназначен для идентификатора библиотеки типов, если этот проект открыт для COM
+// [assembly: Guid("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
